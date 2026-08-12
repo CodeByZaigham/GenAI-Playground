012e1c409510024ed3acad18b2bc123c69b90fb9
CodeByZaigham <zaigham.shah2004@gmail.com>
2026-08-12 22:46:29 +0500

    simplified logic of toolcalling, human in the loop

--- langchain-tools/toolcalling.py
+++ langchain-tools/toolcalling.py
@@ -2,8 +2,7 @@ from langchain.tools import tool
 from dotenv import load_dotenv
 load_dotenv()
 from langchain_mistralai import ChatMistralAI
-from langchain_core.prompts import ChatPromptTemplate,MessagesPlaceholder
-from langchain_core.messages import HumanMessage,ToolMessage
+from langchain_core.messages import HumanMessage
 from rich import print
 
 model = ChatMistralAI(model="mistral-medium-latest")
@@ -22,36 +21,19 @@ tools={
      "weather":weather
 }
 
+messages=[]
+
 model_with_tool=model.bind_tools([weather])
-
-prompt1=HumanMessage(
-     content="what is the weather in karachi today?"
-)
-
-response=model_with_tool.invoke([prompt1])
-
-tool_result=[]
-for i in response.tool_calls:
-     name=i["name"]
-     args=i["args"]
-     func=tools[name]
-     result=func.invoke(args)
-     tool_result.append(ToolMessage(content=str(result) , tool_call_id=i["id"]))
-
-prompt2=ChatPromptTemplate.from_messages([
-     ("system","you are a helpful assistant"),
-     MessagesPlaceholder("chat_history")
-])
-
-final_prompt=prompt2.invoke({
-     "chat_history":[
-          prompt1,
-          response,
-          *tool_result
-     ]
-})
-
-response=model.invoke(final_prompt)
-print(response.content)
+prompt=HumanMessage("what is the weather in karachi today?")
+messages.append(prompt)
+response=model_with_tool.invoke([prompt])
+messages.append(response)
+if response.tool_calls:
+     for i in response.tool_calls:
+          name=i["name"]
+          result=tools[name].invoke(i)
+messages.append(result)
+output=model_with_tool.invoke(messages)
+print(output.content)
 
           
